f6917f769c2d6aa5476c7569bd73e2962356a1e5
develop66er <rondam@yandex.ru>
2016-03-12 20:58:30 +0200

    test

--- src/main/java/CalendarGenerator.java
+++ src/main/java/CalendarGenerator.java
@@ -1,14 +1,11 @@
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 public class CalendarGenerator {
     private static Calendar current;
     private Calendar last;
-    private SimpleDateFormat dateFormat;
     public CalendarGenerator(int startYear){
         last = new GregorianCalendar();
-        dateFormat = new SimpleDateFormat("dd.MM.yyyy");
         current = new GregorianCalendar(startYear,12,0);
     }
     public Calendar generateData() {
